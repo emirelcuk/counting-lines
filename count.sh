472c7610d353cbd18d252f9460a987e6868e5273
Mikhail Epatko <m.epatko@jsa-group.ru>
2024-03-03 00:34:08 +0300

    Added lines counter

--- count.sh
+++ count.sh
@@ -1,3 +1,27 @@
-echo 'Counting lines of code...'
-pwd
-echo '111 lines'
+#!/bin/bash
+
+# ---------------------------------------------
+#  Count lines in the files excluding blank lines
+#
+#  @1 = files to include (posix-egrep regex)
+#  @2 = files to exclude (posix-egrep regex)
+#
+#  EXAMPLES OF USAGE:
+#    - count in all files:
+#      ./count.sh '.*\**' ''
+#
+#    - count in kotlin source files (*.kt):
+#      ./count.sh '.*\.kt' ''
+#
+#    - count in kotlin and bash script files (*.kt, *.sh):
+#      ./count.sh '.*\.(kt|sh)$' ''
+#
+#    - count in all files excluding bash script files (*.sh):
+#      ./count.sh '.*\**' '.*\.sh'
+# ---------------------------------------------
+total=0
+while read -r FILE; do
+  count=$(grep -cve '^\s*$' < "$FILE");
+  (( total+=count ));
+done < <(find . -type f -regextype posix-egrep -regex "$1" ! -regex "$2")
+echo "$total"
